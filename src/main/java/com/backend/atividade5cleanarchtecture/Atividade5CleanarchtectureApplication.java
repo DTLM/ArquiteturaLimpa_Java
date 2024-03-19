7693a47f50fe800f651b4c59ee8bb474165759ee
Thiago Martins <thiagomartins@audax.mobi>
2024-03-19 18:41:26 -0300

    primeiro commit

---
+++ src/main/java/com/backend/atividade5cleanarchtecture/Atividade5CleanarchtectureApplication.java
@@ -0,0 +1,13 @@
+package com.backend.atividade5cleanarchtecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade5CleanarchtectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade5CleanarchtectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/atividade5cleanarchtecture/Atividade5CleanarchtectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.atividade5cleanarchtecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade5CleanarchtectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
